d40ea260720e58821e961da4b876ed70658622f9
unknown <gilbertgalvannn@gmail.com>
2024-01-04 11:20:50 -0800

    pivoting and making this directory a snake game

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,54 +1,40 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
+using namespace sf;
+
+int N = 30, M = 20;
+int size = 20;
+int w = size * N;
+int h = size * M;
 
 int main() {
 
-    /*sf::Window window(sf::VideoMode(800, 600), "Test Window");
+    RenderWindow window(VideoMode(w, h), "Snake Game!");
 
-    while (window.isOpen()) {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-            window.close();
-        }
-    }
-    */
+    Texture t1, t2;
+    t1.loadFromFile("images/white.png");
+    t2.loadFromFile("images/red.png");
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Bouncing Ball");
+    Sprite sprite1(t1);
+    Sprite sprite2(t2);
 
-    // Create a ball shape
-    sf::CircleShape ball(20.f);
-    ball.setFillColor(sf::Color::Green);
-
-    // Set the initial position and velocity of the ball
-    sf::Vector2f position(100.f, 100.f);
-    sf::Vector2f velocity(3.f, 2.f);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+    while (window.isOpen())
+    {
+        Event e;
+        while (window.pollEvent(e))
+        {
+            if (e.type == Event::Closed)
                 window.close();
         }
 
-        // Update the position of the ball
-        position += velocity;
-
-        // Bounce off the window edges
-        if (position.x < 0 || position.x > window.getSize().x - 2 * ball.getRadius())
-            velocity.x = -velocity.x;
-
-        if (position.y < 0 || position.y > window.getSize().y - 2 * ball.getRadius())
-            velocity.y = -velocity.y;
-
-        // Clear the window
         window.clear();
 
-        // Draw the ball
-        ball.setPosition(position);
-        window.draw(ball);
-
-        // Display the contents of the window
-        window.display();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++)
+            {
+                sprite1.setPosition(i * size, j * size);  window.draw(sprite1);
+            }
+            window.display();
+        }
     }
-
     return 0;
 }
