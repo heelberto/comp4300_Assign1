527e9e162776c990953af5f626164cb1148cfa88
unknown <gilbertgalvannn@gmail.com>
2024-01-04 18:53:59 -0800

    sets up window, need to fix the refresh rate as it currently flickers too quickly

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -8,17 +8,23 @@ int h = size * M;
 
 int main() {
 
+    //create a window variable
+    //with WxH dimesions
     RenderWindow window(VideoMode(w, h), "Snake Game!");
 
+    //create 2 textures from the white/red images
     Texture t1, t2;
     t1.loadFromFile("images/white.png");
     t2.loadFromFile("images/red.png");
 
+    //create a sprite w/ the texture of t1/t2
     Sprite sprite1(t1);
     Sprite sprite2(t2);
 
+    //main loop
     while (window.isOpen())
     {
+        //create a simple type of event to capture the window.close()
         Event e;
         while (window.pollEvent(e))
         {
